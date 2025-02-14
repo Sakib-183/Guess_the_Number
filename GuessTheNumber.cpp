cc3eb5f7ffca97883f010be2336663118efd03a5
Sakib-183 <sadman.sakib4400@gmail.com>
2025-02-14 12:39:36 +0600

    first commit

---
+++ GuessTheNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main()
+{
+    int choice;
+    cout << "Welcome to 'Guess the Number' Game!" << endl;
+    cout << "Select difficulty level:" << endl;
+    cout << "1. Easy (1-100, 10 attempts)" << endl;
+    cout << "2. Medium (1-500, 7 attempts)" << endl;
+    cout << "3. Hard (1-1000, 5 attempts)" << endl;
+    cout << "Enter your choice (1/2/3): ";
+    cin >> choice;
+ 
+    return 0;
+
+}
